0abb5ca3593164481ba3df54c063d5a81e96ed67
lanzjasper <lanceangelojasperdominguez@gmail.com>
2022-02-06 07:41:47 +0800

    changes

--- src/pages/UpdatesAndAnnouncements.js
+++ src/pages/UpdatesAndAnnouncements.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 const UpdatesAndAnnouncements = () => {
   const [news, setNews] = useState([]);
   const [comments, setComments] = useState({});
+  const [isGettingNews, setIsGettingNews] = useState(true);
   const styles = {
     newsDescriptionStyle: {
       wordBreak: 'break-all'
@@ -65,6 +66,7 @@ const UpdatesAndAnnouncements = () => {
   };
 
   useEffect(() => {
+    setIsGettingNews(true);
     axios.get('/.netlify/functions/news').then((res) => {
       let news = res.data.data;
 
@@ -79,6 +81,7 @@ const UpdatesAndAnnouncements = () => {
       });
 
       setNews(news);
+      setIsGettingNews(false);
     });
 
     return () => {};
@@ -133,114 +136,139 @@ const UpdatesAndAnnouncements = () => {
     <>
       <HomeNavigation />
       <div className="container-fluid">
-        {news.map((_news) => {
-          return (
-            <div className="row" key={_news.news_id}>
-              <div className="col s12">
-                <div className="card grey lighten-4">
-                  <div className="card-content black-text">
-                    <div className="row valign-wrapper">
-                      <div className="col s2">
-                        <img
-                          src={`https://i.pravatar.cc/150?u=${_news.admin_id}`}
-                          alt=""
-                          className="circle responsive-img"
-                        />
-                      </div>
-                      <div className="col s10">
-                        <span className="black-text">
-                          {`${_news.admin_fname} ${_news.admin_mname} ${_news.admin_lname}`}
-                        </span>
-                        <br></br>
-                        <span>{_news.news_create}</span>
-                      </div>
-                    </div>
-                    <span className="card-title">{_news.news_title}</span>
-                    <p
-                      style={styles.newsDescriptionStyle}
-                      className="color-grey"
-                    >
-                      {_news.news_description}
-                    </p>
-                    <br></br>
-                    <span>Likes: {_news.likeCount}</span>
-                    <br></br>
-                    <span>Comments: {_news.commentsData.length}</span>
+        <div className="row">
+          <div className="col s12">
+            <h5>Updates and Announcements</h5>
+          </div>
+        </div>
+        {isGettingNews && (
+          <div className="col s12">
+            <div className="center-loader-container">
+              <div className="preloader-wrapper big active">
+                <div className="spinner-layer spinner-blue-only">
+                  <div className="circle-clipper left">
+                    <div className="circle"></div>
                   </div>
-                  <div className="card-action">
-                    {/* eslint-disable-next-line */}
-                    <a
-                      href="#"
-                      onClick={(e) => {
-                        like(_news.news_id, e, _news.didCurrentUserLike);
-                      }}
-                      className="black-text"
-                      style={
-                        checkIfUserLikedNews(_news.likesData)
-                          ? styles.bold
-                          : styles.normalFontWeight
-                      }
-                    >
-                      Like
-                    </a>
-                    {/* eslint-disable-next-line */}
-                    <a href="#" className="black-text">
-                      Share
-                    </a>
-                    <ul className="collection" style={{ marginTop: 20 }}>
-                      {_news.commentsData.map((comment) => {
-                        return (
-                          <li
-                            className="collection-item avatar grey lighten-4"
-                            key={comment.comment_id}
-                          >
-                            <img
-                              src={`https://i.pravatar.cc/150?u=admin${comment.userid}`}
-                              alt=""
-                              className="circle"
-                            />
-                            <p>
-                              {`${comment.user_fname} ${comment.user_mname} ${comment.user_lname}`}{' '}
-                              <br />
-                              {comment.comment}
-                              <br />
-                              {comment.date_commented}
-                            </p>
-                          </li>
-                        );
-                      })}
-                      <li className="collection-item avatar grey lighten-4">
-                        <div className="row" style={{ marginBottom: 0 }}>
-                          <div className="input-field col s12">
-                            <textarea
-                              id="newsComment"
-                              className="materialize-textarea"
-                              onKeyDown={(e) => {
-                                addComment(e, _news.news_id);
-                              }}
-                              onChange={(e) => {
-                                setComments({
-                                  ...comments,
-                                  ...{
-                                    [_news.news_id]: e.target.value
-                                  }
-                                });
-                              }}
-                              value={comments[_news.news_id]}
-                            ></textarea>
-                            <label htmlFor="newsComment">
-                              Press enter to add comment
-                            </label>
-                          </div>
-                        </div>
-                      </li>
-                    </ul>
+                  <div className="gap-patch">
+                    <div className="circle"></div>
+                  </div>
+                  <div className="circle-clipper right">
+                    <div className="circle"></div>
                   </div>
                 </div>
               </div>
             </div>
-          );
-        })}
+          </div>
+        )}
+        {!isGettingNews &&
+          news.map((_news) => {
+            return (
+              <div className="row" key={_news.news_id}>
+                <div className="col s12">
+                  <div className="card grey lighten-4">
+                    <div className="card-content black-text">
+                      <div className="row valign-wrapper">
+                        <div className="col s2">
+                          <img
+                            src={`https://i.pravatar.cc/150?u=${_news.admin_id}`}
+                            alt=""
+                            className="circle responsive-img"
+                          />
+                        </div>
+                        <div className="col s10">
+                          <span className="black-text">
+                            {`${_news.admin_fname} ${_news.admin_mname} ${_news.admin_lname}`}
+                          </span>
+                          <br></br>
+                          <span>{_news.news_create}</span>
+                        </div>
+                      </div>
+                      <span className="card-title">{_news.news_title}</span>
+                      <p
+                        style={styles.newsDescriptionStyle}
+                        className="color-grey"
+                      >
+                        {_news.news_description}
+                      </p>
+                      <br></br>
+                      <span>Likes: {_news.likeCount}</span>
+                      <br></br>
+                      <span>Comments: {_news.commentsData.length}</span>
+                    </div>
+                    <div className="card-action">
+                      {/* eslint-disable-next-line */}
+                      <a
+                        href="#"
+                        onClick={(e) => {
+                          like(_news.news_id, e, _news.didCurrentUserLike);
+                        }}
+                        className="black-text"
+                        style={
+                          checkIfUserLikedNews(_news.likesData)
+                            ? styles.bold
+                            : styles.normalFontWeight
+                        }
+                      >
+                        Like
+                      </a>
+                      {/* eslint-disable-next-line */}
+                      <a href="#" className="black-text">
+                        Share
+                      </a>
+                      <ul className="collection" style={{ marginTop: 20 }}>
+                        {_news.commentsData.map((comment) => {
+                          return (
+                            <li
+                              className="collection-item avatar grey lighten-4"
+                              key={comment.comment_id}
+                            >
+                              <img
+                                src={`https://i.pravatar.cc/150?u=admin${comment.userid}`}
+                                alt=""
+                                className="circle"
+                              />
+                              <p>
+                                {`${comment.user_fname} ${comment.user_mname} ${comment.user_lname}`}{' '}
+                                <br />
+                                {comment.comment}
+                                <br />
+                                {comment.date_commented}
+                              </p>
+                            </li>
+                          );
+                        })}
+                        <li className="collection-item avatar grey lighten-4">
+                          <div className="row" style={{ marginBottom: 0 }}>
+                            <div className="input-field col s12">
+                              <textarea
+                                id="newsComment"
+                                className="materialize-textarea"
+                                onKeyDown={(e) => {
+                                  addComment(e, _news.news_id);
+                                }}
+                                onChange={(e) => {
+                                  setComments({
+                                    ...comments,
+                                    ...{
+                                      [_news.news_id]: e.target.value
+                                    }
+                                  });
+                                }}
+                                value={comments[_news.news_id]}
+                              ></textarea>
+                              <label htmlFor="newsComment">
+                                Press enter to add comment
+                              </label>
+                            </div>
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
       </div>
     </>
   );
